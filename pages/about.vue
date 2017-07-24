57c7540df19b4c3f42e5a1e21b0b8ef49aa2b6a7
bdebever <baptiste@studioforty9.com>
2017-07-24 08:38:46 +0100

    Https edit

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <resume />
+</template>
+
+<script>
+    import Resume from '~components/Resume.vue'
+
+    export default {
+        components: {
+            Resume
+        },
+        head () {
+            return { title: this.$t('home.title') }
+        },
+
+        method: {
+
+        }
+    }
+</script>
